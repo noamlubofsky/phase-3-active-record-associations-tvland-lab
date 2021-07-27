9b5f693a6eb6a8fd188e27a58039a81da45ea9e8
Noam Lubofsky <noamlubofsky@gmail.com>
2021-07-27 12:55:52 -0400

    commit tree 3b1f1f74847cab084cbceafd3e0b394733ad25ee

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,24 @@
 class Actor < ActiveRecord::Base
-  
+
+    has_many :characters
+    has_many :shows, through: :characters
+
+    # def characters
+    #     self.characters
+    # end
+
+    # def shows
+    #     self.shows
+    # end
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        self.characters.map do |character|
+            "#{character.name} - #{character.show.name}"
+        end
+    end
+
 end
